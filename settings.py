a6a9a06f67e9b9009c81539c5889350058d7fd6c
SjoerdB93 <info@sjoerd.se>
2022-01-18 13:12:01 +0100

    groundwork for settings using json file

--- settings.py
+++ settings.py
@@ -1,34 +1,21 @@
 import CallUI
 from configparser import ConfigParser
+import json
 
 
 def openSettingsdialog(self):
     # This function loads the SampleDB itself. Filling in the neccesary items in the TableWidget
     self.settingsdialog = CallUI.settingsUI()
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    print(config['label'])
     self.settingsdialog.show()
     #Now mydata is a python dictionary
 
-    config = ConfigParser(strict=False)
-
-    config.read('folder//file.ini')
-    config.read('config.ini')
-    config.set('main', 'key1', '1')
-    config.set('main', 'key2', '3')
-    config.set('main', 'key3', '2')
-    config.set('main', 'key4', '5')
-
-    with open('config.ini', 'w') as f:
-        config.write(f)
-
 def showAbout(self):
-    self.aboutWindow = CallUI.aboutWindow()
-    self.aboutWindow.show()
-    config = ConfigParser()
-    config.read('config.ini')
-    print(config.get('main', 'key1'))  # -> "value1"
-    print(config.get('main', 'key2'))  # -> "value2"
-    print(config.get('main', 'key3'))  # -> "value3"
 
-    # getfloat() raises an exception if the value is not a float
-    # getint() and getboolean() also do this for their respective types
-    an_int = config.getint('main', 'key1')
+    config = {"label": "self.samplelist[i].sampleID", "key2": "value2"}
+
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
+
